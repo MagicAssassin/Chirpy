906c97846b80b503726a25817aa985ecfb0caae0
MagicAssassin <eshtonng@gmail.com>
2026-08-26 16:36:44 +0800

    Set Mehtods for Paths

--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
 func main () {
+	var apiCfg apiConfig
+
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("GET /healthz", handlerHealtz)
+	mux.HandleFunc("GET /metrics", apiCfg.handlerGetNumOfReq)
+	mux.HandleFunc("POST /reset", apiCfg.handlerCfgReset)
 
 	server := http.Server{Handler: mux, Addr: ":8080"}
 
@@ -16,4 +22,36 @@ func main () {
 		fmt.Println(err)
 	}
 }
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w,r)
+	})
+}
+
+func (cfg *apiConfig) handlerGetNumOfReq(w http.ResponseWriter, req *http.Request) {
+	theString := fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())
+	w.Write([]byte(theString))
+}
+
+func (cfg *apiConfig) handlerCfgReset(w http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits.Store(0)
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset to 0"))
+}
+
+func handlerHealtz(w http.ResponseWriter, req *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
+
+
 
