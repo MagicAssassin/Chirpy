126e36626b5d96473f124e9091a5c25873e50db6
MagicAssassin <eshtonng@gmail.com>
2026-08-26 13:39:45 +0800

    added index.html

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main () {
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
+	server := http.Server{Handler: mux, Addr: ":8080"}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
